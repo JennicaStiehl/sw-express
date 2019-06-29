cc9e3ed65d33a598a888c7fbe3aa426854f3c2d0
JennicaStiehl <jennica.stiehl@gmail.com>
2019-06-29 14:38:17 -0600

    adds user functionality, including create, index and show api calls

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const path = require('path');
+const logger = require('../middleware/logger');
+const users = require('./api/v1/users');
+// const router = express.Router();
+const app = express();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//init middleware
+// app.use(logger);
 
-module.exports = router;
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//users api routes
+app.use(users, require('./api/v1/users'));
+// app.use(users, require('./api/v1/users'))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {console.log(`Server started on ${PORT}`)});
